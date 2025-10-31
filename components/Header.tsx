ce37fa108ee977630b47d242be8a0d5a18ea0c2e
victor-praise <61267951+victor-praise@users.noreply.github.com>
2025-10-30 21:53:22 -0400

    Refactor Header component to include user authentication buttons and improve layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,12 +2,14 @@
 
 import Link from "next/link";
 import AgentPulse from "./AgentPulse";
-import { SignedIn } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
 
 function Header() {
   return (
     <header className="sticky top-0 left-0 right-0 px-4 md:px-0 bg-white/80 backdrop-blur-sm border-b border-gray-200 z-50">
         <div className="container mx-auto">
+            <div className="flex items-center justify-between h-16">
             <div className="flex items-center justify-between h-16">
                 <Link href="/" className="flex items-center gap-4">
                 <AgentPulse size="small" color="blue"/>
@@ -15,10 +17,26 @@ function Header() {
                 </Link>
             </div>
 
-            <div>
+            <div className="flex items-center gap-4">
                 <SignedIn>
-                    <Link href="/manage-plan">Manage Plan</Link>
+                    <Link href="/manage-plan">
+                    <Button variant="outline" className="mr-4 bg-linear-to-r from-blue-600 to-blue-400 text-transparent bg-clip-text">
+                        
+                        Manage Plan
+                        </Button></Link>
+
+                        <div className="p-2 w-10 h-10 flex items-center justify-center rounded-full border bg-blue-100 border-blue-200">
+                            <UserButton/>
+                        </div>
                 </SignedIn>
+
+                <SignedOut>
+                    <SignInButton mode="modal">
+                        <Button variant="link" className="bg-linear-to-r from-blue-600 to-blue-400 text-transparent bg-clip-text">Sign in</Button>
+                        </SignInButton>
+                </SignedOut>
+
+            </div>
             </div>
         </div>
     </header>
