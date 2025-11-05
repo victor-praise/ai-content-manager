6b898e60597173627e3b9881bbf00bcb43c72d0b
victor-praise <61267951+victor-praise@users.noreply.github.com>
2025-11-04 21:55:10 -0500

    integrated with schematic

--- components/ClientWrapper.tsx
+++ components/ClientWrapper.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 import { ClerkProvider } from "@clerk/nextjs";
+import { SchematicProvider } from "@schematichq/schematic-react";
 
 
 
@@ -11,9 +12,17 @@ export default function ClientWapper({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const schematicPubKey = process.env.NEXT_PUBLIC_SCHEMATIC_PUBLISHABLE_KEY;
+  if(!schematicPubKey){
+    throw new Error("Schematic publishable key is not defined");
+  }
   return (
     <ClerkProvider>
-      {children}
+      <SchematicProvider publishableKey = {schematicPubKey}>
+{children}
+      </SchematicProvider>
+      
     </ClerkProvider>
   );
 }
