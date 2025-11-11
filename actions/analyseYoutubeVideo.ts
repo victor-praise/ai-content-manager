17357caefa3ef60cc478bb6e826742f472e714a3
victor-praise <61267951+victor-praise@users.noreply.github.com>
2025-11-10 22:31:15 -0500

    Analyze video page

--- actions/analyseYoutubeVideo.ts
+++ actions/analyseYoutubeVideo.ts
@@ -1,11 +1,17 @@
 "use server";
 
+import { getVideoFromUrl } from "@/lib/getVideoFromUrl";
 import {redirect} from "next/navigation";
 
 export async function analyseYoutubeVideo(formData: FormData) {
     const url = formData.get("url")?.toString();
 
-    const videoId = "abc";
+    if (!url) return;
+
+    const videoId = getVideoFromUrl(url);
+
+    console.log("Extracted video ID:", videoId);
+    
     if(!videoId) return;
 
     redirect(`/video/${videoId}/analysis`);
---
+++ lib/getVideoFromUrl.ts
@@ -0,0 +1,15 @@
+export function getVideoFromUrl(url: string): string | null {
+    let videoId: string | null = null;
+
+    if(url.includes("youtu.be/")){
+        videoId = url.split("youtu.be/")[1].split(/[?#]/)[0] || null;
+    } 
+    else if(url.includes("youtube.com/shorts/")){
+        videoId=url.split("shorts/")[1]?.split(/[?#]/)[0] || null;
+    }
+    else if(url.includes("v=")){
+        videoId = url.split("v=")[1]?.split("&")[0] || null;
+    }
+
+    return videoId;
+}
